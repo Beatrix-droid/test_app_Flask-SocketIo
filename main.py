c3b0f36624d1b5f0163265dba7b17b7da7d77c5d
Beatrix-droid <amilcare8@gmail.com>
2022-01-08 20:54:23 +0000

    initial set up and 'on connect' function has bene implemented

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask_socketio import SocketIO, send
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["SECRET KEY"] = "a_secret!"
+socketio = SocketIO(app)
+
+
+# defining a message:
+
+@socketio.on("message")
+def handleMessage(msg):
+	print("message: " + msg)
+	# broadcast = true lets everyone in the chatroom see the message
+	send(msg, broadcast=True)
+
+@app.route('/')
+def hello_world():
+	return render_template("index.html")
+
+if __name__ == "__main_":
+	# instead of app.run()
+	# socket io takes the typical flask app and wraps it around the socketio functionilty
+	socketio.run(app)
